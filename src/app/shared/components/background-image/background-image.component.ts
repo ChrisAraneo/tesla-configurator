f2d209efb4e25b0b21e62f26faf74802e7d6d7b8
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-14 19:01:53 +0200

    Implement BackgroundImage

--- src/app/shared/components/background-image/background-image.component.ts
+++ src/app/shared/components/background-image/background-image.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage, NgStyle } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-background-image',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, NgStyle],
   templateUrl: './background-image.component.html',
   styleUrl: './background-image.component.scss',
 })
-export class BackgroundImageComponent {}
+export class BackgroundImageComponent implements AfterViewInit {
+  @Input({ required: true }) src: string = '';
+
+  @ViewChild('image') imageRef?: ElementRef;
+
+  protected isLoading: boolean = true;
+
+  ngAfterViewInit(): void {
+    const nativeImageElement: HTMLImageElement | undefined = this.imageRef?.nativeElement;
+
+    if (nativeImageElement) {
+      // TODO Destroy??
+      nativeImageElement.addEventListener('load', () => {
+        this.isLoading = false;
+      });
+      nativeImageElement.addEventListener('error', () => {
+        // TODO Handle error
+      });
+    }
+  }
+}
