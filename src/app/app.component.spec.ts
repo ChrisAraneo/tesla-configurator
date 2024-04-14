977d5784ed30bfc937242b4ddd371ed1d1cce80c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-14 15:52:55 +0200

    Remove starter code, update unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tesla-configurator' title`, () => {
+  it(`should have the 'AppComponent works!' text`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tesla-configurator');
+    expect(app.text).toEqual('AppComponent works!');
   });
 
-  it('should render title', () => {
+  it('should render text', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tesla-configurator');
+    expect(compiled.querySelector('h1')?.textContent).toContain('AppComponent works!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
   selector: 'app-root',
   standalone: true,
   imports: [AsyncPipe, JsonPipe],
-  template: ` <h1>Hello from {{ name }}!</h1> `,
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.scss',
 })
 export class AppComponent {
-  name = 'Angular';
+  text = 'AppComponent works!';
 }
