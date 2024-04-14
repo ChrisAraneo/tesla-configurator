6a84eaf303fff648008650235854c480bc060d2f
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-14 16:26:31 +0200

    Add home route and page not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Tesla Configurator',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    title: 'Tesla Configurator - Page not found',
+    component: NotFoundComponent,
+  },
+];
