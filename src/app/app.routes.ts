5b3f099476a067f20fbf9457690b485b7aeee087
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 21:51:17 +0200

    Organize imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
