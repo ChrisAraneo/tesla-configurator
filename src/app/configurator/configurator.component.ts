9d9a72eb73304a942c804bb0cb3acf3f2a74d0b3
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 21:52:41 +0200

    Create ConfiguratorComponent

---
+++ src/app/configurator/configurator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configurator',
+  standalone: true,
+  imports: [],
+  templateUrl: './configurator.component.html',
+  styleUrl: './configurator.component.scss',
+})
+export class ConfiguratorComponent {}
