dfeeb7d5ed8ccbb0c5b69847ce246600919f85ec
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 22:52:54 +0200

    Add configurator routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ConfiguratorComponent } from './configurator/configurator.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -7,6 +8,15 @@ export const routes: Routes = [
     path: '',
     title: 'Tesla Configurator',
     component: HomeComponent,
+  },
+  {
+    path: 'configurator/:step',
+    title: 'Tesla Configurator - Choose model & color',
+    component: ConfiguratorComponent,
+  },
+  {
+    path: 'configurator',
+    redirectTo: 'configurator/1',
   },
   {
     path: '**',
