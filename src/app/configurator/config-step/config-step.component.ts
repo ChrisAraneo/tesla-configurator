261e7325cf34b4cf8c88044e12275f088b4ee1d6
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 23:20:04 +0200

    Create ConfigStepComponent

---
+++ src/app/configurator/config-step/config-step.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './config-step.component.html',
+  styleUrl: './config-step.component.scss',
+})
+export class ConfigStepComponent {}
