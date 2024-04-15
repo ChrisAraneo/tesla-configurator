ac75896110ac73610c01e0dba8296bab4aa7f936
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 22:31:55 +0200

    Implement PageComponent

--- src/app/shared/components/page/page.component.spec.ts
+++ src/app/shared/components/page/page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PageComponent } from './page.component';
 
+// TODO Test content projection
 describe('PageComponent', () => {
   let component: PageComponent;
   let fixture: ComponentFixture<PageComponent>;
--- src/app/shared/components/page/page.component.ts
+++ src/app/shared/components/page/page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BackgroundImageComponent } from '../background-image/background-image.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [],
+  imports: [BackgroundImageComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss',
 })
-export class PageComponent {}
+export class PageComponent {
+  @Input({ required: true }) backgroundImageSrc: string = '';
+}
