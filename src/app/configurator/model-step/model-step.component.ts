46fb3662ea98510f570205095987985f6cf9fa2c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 23:19:28 +0200

    Create ModelStepComponent

---
+++ src/app/configurator/model-step/model-step.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './model-step.component.html',
+  styleUrl: './model-step.component.scss',
+})
+export class ModelStepComponent {}
