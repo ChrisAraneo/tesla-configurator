46447101b12b2c3a6e670507484874d16cd44608
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-15 21:46:41 +0200

    Implement NotFoundComponent

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BackgroundImageComponent } from '../shared/components/background-image/background-image.component';
+import { TitleComponent } from '../shared/components/title/title.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [BackgroundImageComponent, TitleComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
