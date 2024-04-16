d34a2587474d0c0d4abf14624d5203a95812baf6
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-17 00:06:04 +0200

    Implement ImageComponent

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-image',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss',
 })
-export class ImageComponent {}
+export class ImageComponent implements AfterViewInit {
+  @Input({ required: true }) src: string = '';
+  @Input({ required: true }) width: number = 0;
+  @Input({ required: true }) height: number = 0;
+  @Input({ required: true }) alt: string = '';
+
+  @ViewChild('image') imageRef?: ElementRef;
+
+  protected isLoading: boolean = true;
+
+  ngAfterViewInit(): void {
+    const nativeImageElement: HTMLImageElement | undefined = this.imageRef?.nativeElement;
+
+    if (nativeImageElement) {
+      // TODO Destroy??
+      nativeImageElement.addEventListener('load', () => {
+        this.isLoading = false;
+      });
+      nativeImageElement.addEventListener('error', () => {
+        // TODO Handle error
+      });
+    }
+  }
+}
