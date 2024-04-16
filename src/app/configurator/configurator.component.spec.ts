a693c1a6f9235a3ccd7014449662cfb0e4d15f35
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-16 21:12:47 +0200

    Define basic structure and styles

--- src/app/configurator/configurator.component.spec.ts
+++ src/app/configurator/configurator.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { ConfiguratorComponent } from './configurator.component';
 
 describe('ConfiguratorComponent', () => {
@@ -8,6 +9,12 @@ describe('ConfiguratorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ConfiguratorComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfiguratorComponent);
--- src/app/configurator/configurator.component.ts
+++ src/app/configurator/configurator.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { PageComponent } from '../shared/components/page/page.component';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-configurator',
   standalone: true,
-  imports: [],
+  imports: [PageComponent, CardComponent, RouterOutlet, RouterLink],
   templateUrl: './configurator.component.html',
   styleUrl: './configurator.component.scss',
 })
