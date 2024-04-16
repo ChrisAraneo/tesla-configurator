235fbfd7529d90661f335139182bf55e2ed2a801
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-16 21:10:09 +0200

    Create ConfiguratorService

---
+++ src/app/configurator/configurator.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { ConfiguratorService } from './configurator.service';
+
+describe('ConfiguratorService', () => {
+  let service: ConfiguratorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ConfiguratorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
