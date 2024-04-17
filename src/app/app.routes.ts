1b4d7eb15341c7a0dcafe249b5ffac40455e081e
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-17 23:03:36 +0200

    Declare configurator routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConfigStepComponent } from './configurator/config-step/config-step.component';
 import { ConfiguratorComponent } from './configurator/configurator.component';
+import { ModelStepComponent } from './configurator/model-step/model-step.component';
+import { SummaryStepComponent } from './configurator/summary-step/summary-step.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -10,13 +13,27 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'configurator/:step',
-    title: 'Tesla Configurator - Choose model & color',
-    component: ConfiguratorComponent,
-  },
-  {
+    // TODO Guards
     path: 'configurator',
-    redirectTo: 'configurator/1',
+    component: ConfiguratorComponent,
+    children: [
+      // TODO Guards
+      {
+        path: '1',
+        title: 'Tesla Configurator - Step 1 - Choose model & color',
+        component: ModelStepComponent,
+      },
+      {
+        path: '2',
+        title: 'Tesla Configurator -  Step 2 - Select config & options',
+        component: ConfigStepComponent,
+      },
+      {
+        path: '3',
+        title: 'Tesla Configurator -  Step 3 - Summary',
+        component: SummaryStepComponent,
+      },
+    ],
   },
   {
     path: '**',
