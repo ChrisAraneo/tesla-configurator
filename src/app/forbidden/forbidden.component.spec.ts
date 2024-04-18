ede0c574fe777e8da1b196d9b19a7d819feda6f7
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 20:59:10 +0200

    Update component structure and unit tests

--- src/app/forbidden/forbidden.component.spec.ts
+++ src/app/forbidden/forbidden.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { ForbiddenComponent } from './forbidden.component';
 
-// TODO Unit tests
 describe('ForbiddenComponent', () => {
   let component: ForbiddenComponent;
   let fixture: ComponentFixture<ForbiddenComponent>;
@@ -9,6 +9,12 @@ describe('ForbiddenComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ForbiddenComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ForbiddenComponent);
@@ -18,5 +24,29 @@ describe('ForbiddenComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render header', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('h1')?.textContent).toContain(
+      'Cannot access',
+    );
+  });
+
+  it('should render paragraph', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('p')?.textContent).toContain(
+      'You cannot access this page because your session expired or required data in the form is missing.',
+    );
+  });
+
+  it('should render first link with correct text', () => {
+    expect(fixture.debugElement.nativeElement.querySelectorAll('a')[0]?.textContent).toContain(
+      'Go to Tesla configurator',
+    );
+  });
+
+  it('should render second link with correct text', () => {
+    expect(fixture.debugElement.nativeElement.querySelectorAll('a')[1]?.textContent).toContain(
+      'Homepage',
+    );
   });
 });
--- src/app/forbidden/forbidden.component.ts
+++ src/app/forbidden/forbidden.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PageComponent } from '../shared/components/page/page.component';
+import { ParagraphComponent } from '../shared/components/paragraph/paragraph.component';
 import { TitleComponent } from '../shared/components/title/title.component';
 
 @Component({
   selector: 'app-forbidden',
   standalone: true,
-  imports: [PageComponent, TitleComponent, RouterLink],
+  imports: [PageComponent, TitleComponent, ParagraphComponent, RouterLink],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss',
 })
