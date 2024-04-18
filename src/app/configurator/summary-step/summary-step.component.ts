4b6b59ac5acec5c0fe7281e085846a16de344147
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 17:27:32 +0200

    Implement SummaryStep

--- src/app/configurator/summary-step/summary-step.component.ts
+++ src/app/configurator/summary-step/summary-step.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable, combineLatest, map } from 'rxjs';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { Color } from '../../shared/services/color.type';
+import { Config } from '../../shared/services/config.type';
+import { Model } from '../../shared/services/model.type';
+import { ConfiguratorService } from '../configurator.service';
 
 @Component({
   selector: 'app-summary-step',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, AsyncPipe, NgIf],
   templateUrl: './summary-step.component.html',
   styleUrl: './summary-step.component.scss',
 })
-export class SummaryStepComponent {}
+export class SummaryStepComponent implements OnInit {
+  loading!: Observable<boolean>;
+  model!: Observable<Model>;
+  config!: Observable<Config>;
+  color!: Observable<Color>;
+  towHitch!: Observable<boolean>;
+  yoke!: Observable<boolean>;
+
+  total!: Observable<number>;
+
+  constructor(private readonly service: ConfiguratorService) {}
+
+  ngOnInit(): void {
+    this.loading = this.service.loading;
+    this.model = this.service.selectedModel;
+    this.config = this.service.selectedConfig;
+    this.color = this.service.selectedColor;
+    this.towHitch = this.service.towHitch;
+    this.yoke = this.service.yoke;
+
+    // TODO Move calculating total to service
+    this.total = combineLatest([this.config, this.color, this.towHitch, this.yoke]).pipe(
+      map(([config, color, towHitch, yoke]) => {
+        // TODO Implement rule on additional 1000$s
+        return config.price + color.price + (towHitch ? 1000 : 0) + (yoke ? 1000 : 0);
+      }),
+    );
+  }
+}
