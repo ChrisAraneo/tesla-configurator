5f44a678acf66e57d59980bd6485a54dd43ca63d
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 21:11:37 +0200

    Create ErrorCardComponent

---
+++ src/app/shared/components/error-card/error-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-card.component.html',
+  styleUrl: './error-card.component.scss',
+})
+export class ErrorCardComponent {}
