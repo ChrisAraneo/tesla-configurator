cbc6167fb72e2915f038eac20a13a33ec21f899c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 17:55:22 +0200

    Add guard to ConfigStep route, add route with redirecting on parent Configurator route, add forbidden route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { ConfigStepComponent } from './configurator/config-step/config-step.comp
 import { ConfiguratorComponent } from './configurator/configurator.component';
 import { ModelStepComponent } from './configurator/model-step/model-step.component';
 import { SummaryStepComponent } from './configurator/summary-step/summary-step.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { canActivateConfigStep } from './shared/guards/can-activate-config-step.guard';
 
 export const routes: Routes = [
   {
@@ -25,6 +27,7 @@ export const routes: Routes = [
       },
       {
         path: '2',
+        canActivate: [canActivateConfigStep],
         title: 'Tesla Configurator -  Step 2 - Select config & options',
         component: ConfigStepComponent,
       },
@@ -33,7 +36,17 @@ export const routes: Routes = [
         title: 'Tesla Configurator -  Step 3 - Summary',
         component: SummaryStepComponent,
       },
+      {
+        path: '',
+        pathMatch: 'prefix',
+        redirectTo: '1',
+      },
     ],
+  },
+  {
+    path: 'forbidden',
+    title: 'Tesla Configurator - Forbidden',
+    component: ForbiddenComponent,
   },
   {
     path: '**',
