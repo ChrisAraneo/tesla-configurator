15b2c23f31d0fd53727e80085b528fa2ed683617
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 17:16:02 +0200

    Implement #getOptions

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,19 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, retry } from 'rxjs';
 import { ModelsApiResponse } from './models-api-response.type';
+import { OptionsApiResponse } from './options-api-response.type';
 
 const MODELS_ENDPOINT: string = '/models'; // TODO Move to env/providers
+const OPTIONS_ENDPOINT: string = '/options'; // TODO Move to env/providers
 
 // TODO Map to success or error
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  private readonly retryCount = 10;
+  private readonly retryDelay = 250;
+
   constructor(private httpClient: HttpClient) {}
 
   getModels(): Observable<ModelsApiResponse> {
     return this.httpClient
       .get<ModelsApiResponse>(MODELS_ENDPOINT)
-      .pipe(retry({ count: 10, delay: 250 }));
+      .pipe(retry({ count: this.retryCount, delay: this.retryDelay }));
+  }
+
+  getOptions(modelCode: string): Observable<OptionsApiResponse> {
+    return this.httpClient
+      .get<OptionsApiResponse>(`${OPTIONS_ENDPOINT}/${modelCode}`)
+      .pipe(retry({ count: this.retryCount, delay: this.retryDelay }));
   }
 }
