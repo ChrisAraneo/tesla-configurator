7f0b90a55a913e651620da3c904de47d6ff1a0cd
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 18:16:24 +0200

    Create and implement canActivateSummaryStep

---
+++ src/app/shared/guards/can-activate-summary-step.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { first, map, mergeMap } from 'rxjs';
+import { ConfiguratorService } from '../../configurator/configurator.service';
+
+// TODO File name? Fix name?
+export const canActivateSummaryStep: CanActivateFn = () => {
+  const service: ConfiguratorService = inject(ConfiguratorService);
+  const router: Router = inject(Router);
+
+  return service.disabledStep.pipe(
+    map((value) => value[3]),
+    first(),
+    mergeMap((isDisabled: boolean): Promise<boolean> => {
+      if (isDisabled) {
+        return router.navigate(['forbidden']);
+      } else {
+        return Promise.resolve(true);
+      }
+    }),
+  );
+};
