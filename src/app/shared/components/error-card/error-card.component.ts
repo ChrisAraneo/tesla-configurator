3f0315881bf23d1dbcc2c3aa4c56d97957cb2854
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 21:28:59 +0200

    Implement ErrorCardComponent

--- src/app/shared/components/error-card/error-card.component.ts
+++ src/app/shared/components/error-card/error-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ParagraphComponent } from '../paragraph/paragraph.component';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-error-card',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, ParagraphComponent, NgIf, RouterLink],
   templateUrl: './error-card.component.html',
   styleUrl: './error-card.component.scss',
 })
-export class ErrorCardComponent {}
+export class ErrorCardComponent {
+  @Input() title: string = '';
+  @Input() message: string = '';
+  @Input() button: string = '';
+  @Input() onClick: (() => void | Promise<void>) | undefined;
+
+  protected readonly warning = '\u26A0';
+}
