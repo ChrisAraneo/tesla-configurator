722017a609a3309220744dff6f1f0dc0e7b326f0
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-18 18:18:33 +0200

    Implement ForbiddenComponent

--- src/app/forbidden/forbidden.component.ts
+++ src/app/forbidden/forbidden.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PageComponent } from '../shared/components/page/page.component';
+import { TitleComponent } from '../shared/components/title/title.component';
 
 @Component({
   selector: 'app-forbidden',
   standalone: true,
-  imports: [],
+  imports: [PageComponent, TitleComponent, RouterLink],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss',
 })
