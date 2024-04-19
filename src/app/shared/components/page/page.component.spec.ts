d0d32446f1917faf01187c331284806c0990bf3e
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-20 00:57:18 +0200

    Add content projection unit test

--- src/app/shared/components/page/page.component.spec.ts
+++ src/app/shared/components/page/page.component.spec.ts
@@ -1,24 +1,35 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PageComponent } from './page.component';
 
-// TODO Test content projection
+@Component({
+  standalone: true,
+  imports: [PageComponent],
+  template: `<app-page [backgroundImageSrc]="'assets/example.jpg'" [backgroundImageAlt]="'Example'">
+    <div id="test">Div inside the page</div>
+  </app-page>`,
+})
+class TestHostComponent {}
+
 describe('PageComponent', () => {
-  let component: PageComponent;
-  let fixture: ComponentFixture<PageComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageComponent],
+      imports: [PageComponent, TestHostComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(PageComponent);
-    component = fixture.componentInstance;
-    component.backgroundImageSrc = 'assets/example.jpg';
-    component.backgroundImageAlt = 'Example';
+    fixture = TestBed.createComponent(TestHostComponent);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create host component', () => {
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should project content into the component', () => {
+    expect(fixture.debugElement.nativeElement.querySelector('#test')?.textContent).toBe(
+      'Div inside the page',
+    );
   });
 });
