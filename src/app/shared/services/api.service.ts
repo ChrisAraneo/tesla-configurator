f209d8b519d649b82be532067ab66e11c0c05446
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-19 21:51:19 +0200

    Use injected endpoints

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, catchError, map, of, retry } from 'rxjs';
+import { ApiEndpoints } from './types/api-endpoints.type';
 import { ModelsApiData } from './types/models-api-data.type';
 import { ModelsApiResponse } from './types/models-api-response.type';
 import { OptionsApiData } from './types/options-api-data.type';
 import { OptionsApiResponse } from './types/options-api-response.type';
 import { Status } from './types/status.type';
-
-const MODELS_ENDPOINT: string = '/models'; // TODO Move to env/providers
-const OPTIONS_ENDPOINT: string = '/options'; // TODO Move to env/providers
 
 @Injectable({
   providedIn: 'root',
@@ -17,10 +15,13 @@ export class ApiService {
   private readonly retryCount = 10;
   private readonly retryDelay = 250;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    @Inject('API') private api: ApiEndpoints,
+    private httpClient: HttpClient,
+  ) {}
 
   getModels(): Observable<ModelsApiResponse> {
-    return this.httpClient.get<ModelsApiData>(MODELS_ENDPOINT).pipe(
+    return this.httpClient.get<ModelsApiData>(this.api.modelsEndpoint).pipe(
       retry({ count: this.retryCount, delay: this.retryDelay }),
       map((response: ModelsApiData) => {
         return {
@@ -39,21 +40,23 @@ export class ApiService {
   }
 
   getOptions(modelCode: string): Observable<OptionsApiResponse> {
-    return this.httpClient.get<OptionsApiData>(`${OPTIONS_ENDPOINT}/${modelCode}`).pipe(
-      retry({ count: this.retryCount, delay: this.retryDelay }),
-      map((response: OptionsApiData) => {
-        return {
-          status: Status.Success,
-          data: response,
-        };
-      }),
-      catchError(() => {
-        return of({
-          status: Status.Error,
-          data: null,
-          message: 'Error while fetching options.',
-        });
-      }),
-    );
+    return this.httpClient
+      .get<OptionsApiData>(this.api.optionsEndpoint.replace(':id', modelCode))
+      .pipe(
+        retry({ count: this.retryCount, delay: this.retryDelay }),
+        map((response: OptionsApiData) => {
+          return {
+            status: Status.Success,
+            data: response,
+          };
+        }),
+        catchError(() => {
+          return of({
+            status: Status.Error,
+            data: null,
+            message: 'Error while fetching options.',
+          });
+        }),
+      );
   }
 }
