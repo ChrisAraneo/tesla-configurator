f3d1c03475f037668fe784540e3fe33efb24e081
Krzysztof Pająk <chris.araneo@gmail.com>
2024-04-19 23:41:20 +0200

    Update providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { ApiService } from './shared/services/api/api.service';
+import { GlobalErrorHandlerService } from './shared/services/global-error-handler/global-error-handler.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: 'API', useValue: environment.api },
+    { provide: ErrorHandler, useClass: GlobalErrorHandlerService },
     provideHttpClient(),
     provideRouter(routes),
+    ApiService,
   ],
 };
